f9c1e95e3dd1ee4cf6ec116def6bee7d550ba666
Wrong <ilak71363@gmail.com>
2022-06-20 06:00:02 +0300

    Начата админ панель

---
+++ WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+
+namespace WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IGameRepository repository;
+        
+        public AdminController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Games);
+        }
+    }
+}
